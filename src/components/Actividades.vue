94b2951bcc62ef3064eaf010d74bc19e7e9f4994
Alejito02 <alejzr2@gmail.com>
2024-10-14 22:01:12 -0500

    Add files via upload

---
+++ src/components/Actividades.vue
@@ -0,0 +1,124 @@
+<template>
+    <div class="containerA">
+     <div class="Principal">
+       <div class="Card1">
+        <img src="https://cl2.buscafs.com/www.tomatazos.com/public/uploads/images/30731/30731.jpg" class="img" height="90%" width="100%">
+        <p class="textCA">Clase de Yoga al Amanecer "Despierta tus sentidos con el sol: Únete a nuestras clases de yoga al amanecer y empieza el día en armonía. 🌅🧘‍♂️"</p>
+        </div>
+
+        <div class="Card2">
+          <img src="https://preview.redd.it/jfawxying9511.jpg?auto=webp&s=235f9376a5a2e4ea352a121f9ad1afd51df51c95" class="img" height="90%" width="100%">
+          <p class="textCA">"¡Desata tu Estrella Interior! Únete a nuestra noche de karaoke y canta tus éxitos favoritos en un ambiente lleno de diversión. 🎤✨"</p>
+        </div>
+
+       <div class="Card3">
+        <img src="https://img.freepik.com/fotos-premium/noche-cine-al-aire-libre-estrellas_1007506-22239.jpg" class="img" height="90%" width="100%">
+        <p class="textCA">Noches de Cine al Aire Libre "Bajo las estrellas: Disfruta de películas clásicas en nuestra terraza con palomitas y vistas al cielo nocturno. 🎬🌌"</p>
+        </div>      
+
+       <div class="Card1">
+        <img src="https://i.blogs.es/06e73c/hannibal8/650_1200.jpg" class="img" height="90%" width="100%">
+        <p class="textCA">Descubre la cocina local: "Explora los sabores auténticos en nuestro restaurante y déjate sorprender por cada bocado. ¡El placer está en el plato!"</p>
+        </div>
+
+        <div class="Card2">
+          <img src="https://images.squarespace-cdn.com/content/v1/659d79bfcbafbe5e3269d464/1646d4c2-868b-41ac-bd00-b918e0759aa6/chris-bumstead-cbum-rutina-hombros.jpg" class="img" height="90%" width="100%">
+          <p class="textCA">Entrena en el gimnasio: "¡Mantente activo incluso en tus vacaciones! Visita nuestro gimnasio y empieza el día con energía renovada."</p>
+        </div>
+
+       <div class="Card3">
+        <img src="https://www.formulatv.com/images/noticias/77100/77146/1_94bcb769b0.jpg" class="img" height="90%" width="100%">
+        <p class="textCA">Explora los alrededores con una caminata guiada: "Conéctate con la naturaleza y la historia local en una caminata guiada. ¡Aventúrate y descubre paisajes únicos!"</p>
+        </div>    
+     </div>
+      <router-view />
+    </div>
+</template>
+<style>
+.containerA{
+ background-image:linear-gradient(rgba(36, 36, 36, 0.452), rgba(180, 168, 168, 0.349)), url(https://images.bauerhosting.com/empire/2023/09/lost-translation-3.jpg?auto=format&w=1440&q=80);
+ background-repeat: no-repeat;
+ background-size: 130%;
+ display: flex;
+ justify-content: center;
+}
+
+.Principal{
+  background-color: rgba(66, 211, 255, 0.452);
+  border-radius: 10px;
+  width: 80%;
+  height: 100vh;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.616);
+  line-height: 2;
+  text-align: center;
+  color: #ffffff;
+  display: grid;
+  grid-template-columns:50% 50%;
+  grid-template-rows: 34% 34% 33% ;;
+  justify-content: center;
+  padding: 1%;
+}
+
+.Card1{
+  background-color: rgba(72, 154, 179, 0.795);
+  border-radius: 10px;
+  width: 90%;
+  height: 90%;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.623);
+  line-height: 2;
+  text-align: center;
+  color: #ffffff;
+  display: grid;
+  grid-template-columns: 50% 50%;
+  margin-left: 5%;
+  margin-top: 2%;
+}
+
+.Card2{
+  background-color: rgba(68, 152, 177, 0.692);
+  border-radius: 10px;
+  width: 90%;
+  height: 90%;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.623);
+  line-height: 2;
+  text-align: center;
+  color: #ffffff;
+  display: grid;
+  grid-template-columns: 50% 50%;
+  margin-left: 5%;
+  margin-top: 2%;
+}
+
+.Card3{
+  background-color: rgba(68, 152, 177, 0.699);
+  border-radius: 10px;
+  width: 90%;
+  height: 90%;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.623);
+  line-height: 2;
+  text-align: center;
+  color: #ffffff;
+  display: grid;
+  grid-template-columns: 50% 50%;
+  margin-left: 5%;
+  margin-top: 2%;
+}
+
+.img{
+  transition: 0.5s;
+  opacity: 0.8;
+}
+
+
+.textCA{
+  font-family: "Manrope", sans-serif;
+}
+.img:hover{
+  transform: scale(1.3);
+}
+
+</style>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap');
+</style>
---
+++ src/components/Habitaciones.vue
@@ -0,0 +1,131 @@
+<template>
+    <div class="containerHB">
+      <div class="CardM">
+        <div class="HabIn">
+         <img src="https://www.hotelmedici.com/images/stories/camere/singola_1200/singola_1_1200.jpg" class="imgIn" height="90%" width="100%">
+         <p class="text">Habitación Individual Perfecta para viajeros de negocios o aquellos que desean relajarse en un ambiente íntimo.</p>
+        </div>
+        <div class="HabDE">
+         <img src="https://images.mirai.com/INFOROOMS/100121951/39rbIWlTZaauLh81PHWd/39rbIWlTZaauLh81PHWd_original.jpg" class="imgDE" height="90%" width="100%">
+         <p class="text">Habitación Doble Estándar Ideal para parejas o quienes buscan más espacio, nuestra habitación doble estándar cuenta con una cómoda cama doble y un ambiente relajante. Disfruta de conexión Wi-Fi gratuita.</p>
+        </div>
+        <div class="HabDD">
+         <img src="https://www.amsterdamforesthotel.com/images/rooms/deluxe-twin/gallery/deluxe-twin-room-amsterdam-forest-hotel-3.jpg" class="imgDD" height="100%" width="100%">
+         <p class="text">Habitación Doble Deluxe Experimenta el lujo y el confort en nuestra Habitación Doble Deluxe. Con una amplia cama doble, elegante diseño y detalles exclusivos para una estancia inolvidable. Disfruta de Wi-Fi de alta velocidad, minibar, y vistas privilegiadas que te invitan a relajarte y disfrutar.</p>
+        </div>
+        <div class="Est">
+         <img src="https://www.hoteloroeljaca.com/wp-content/uploads/habitacion-studio-similares.jpg" class="imgEst" height="90%" width="100%">
+         <p class="text">Habitación Estudio Disfruta de la independencia y el confort en nuestra Habitación Estudio. Con un diseño moderno y funcional, cuenta con una cama cómoda, área de trabajo y kitchenette totalmente equipada.</p>
+        </div>
+        <div class="SuiJ">
+         <img src="https://www.zaanhotel.com/images/rooms/junior-suite/gallery/Junior%20Suite%208.jpg" class="imgJ" height="90%" width="100%">
+         <p class="text">Suite Júnior Vive una experiencia de lujo en nuestra Suite Júnior. Con un amplio espacio, una cama doble de gran confort y una zona de estar separada, es ideal para quienes buscan un toque extra de comodidad.</p>
+        </div>
+        <div class="SuiP">
+         <img src="https://s3.amazonaws.com/static-webstudio-accorhotels-usa-1.wp-ha.fastbooking.com/wp-content/uploads/sites/19/2022/03/11175445/DUF_7063-v-ok-1170x780.jpg" class="imgP" height="100%" width="100%">
+         <p class="text">Suite Presidencial Sumérgete en el máximo lujo y exclusividad en nuestra Suite Presidencial. Con un diseño sofisticado y amplios espacios, ofrece una elegante habitación principal, sala de estar privada, comedor y vistas panorámicas inigualables. Equipada con todas las comodidades de alta gama.</p>
+        </div>
+      </div>
+      <router-view />
+    </div>
+</template>
+
+<style>
+.containerHB{
+  height: 100vh;
+  background-image:linear-gradient(rgba(36, 36, 36, 0.8), rgba(61, 61, 61, 0.808)), url(https://live.staticflickr.com/7688/17849982309_690149079b_b.jpg);
+  background-repeat: no-repeat;
+  background-size: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.CardM{
+  background-color: rgba(90, 201, 235, 0.527);
+  border-radius: 10px;
+  width: 70%;
+  height: 100vh;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  line-height: 2;
+  text-align: center;
+  color: #ffffff;
+  margin-bottom: 5px;
+  display: grid;
+  grid-template-columns:30% 30% 30%;
+  grid-template-rows: 50% 50% ;
+  justify-content: center;
+
+}
+.HabIn{
+    background-color: rgb(86, 141, 158);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.781);
+    margin: 9px;
+    transition: 0.5s;
+    display: grid;
+    grid-auto-rows: 60% 40%;
+}
+.HabDE{
+    background-color: rgb(86, 141, 158);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.781);
+    margin: 9px;
+    transition: 0.5s;
+    display: grid;
+    grid-auto-rows: 60% 40%;    
+}
+.HabDD{
+    background-color: rgb(86, 141, 158);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.781);
+    margin: 9px;
+    transition: 0.5s;  
+    display: grid;
+    grid-auto-rows: 50% 50%;
+}
+.Est{
+    background-color: rgb(86, 141, 158);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.781);
+    margin: 9px;
+    transition: 0.5s;
+    display: grid;
+    grid-auto-rows: 60% 40%;
+}
+.SuiJ{
+    background-color: rgb(86, 141, 158);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.781);
+    margin: 9px;
+    transition: 0.5s;
+    display: grid;
+    grid-auto-rows: 60% 40%;
+}
+.SuiP{
+    background-color: rgb(86, 141, 158);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.781);
+    margin: 9px;
+    transition: 0.5s;
+    display: grid;
+    grid-auto-rows: 50% 50%;
+}
+.HabIn:hover{
+    transform: scale(1.1);
+}
+.HabDE:hover{
+    transform: scale(1.1);
+}
+.HabDD:hover{
+    transform: scale(1.1);
+}
+.Est:hover{
+    transform: scale(1.1);
+}
+.SuiJ:hover{
+    transform: scale(1.1);
+}
+.SuiP:hover{
+    transform: scale(1.1);
+}
+
+.text{
+    font-size: 60%;
+    color: rgb(230, 255, 241);
+}
+</style>
+
